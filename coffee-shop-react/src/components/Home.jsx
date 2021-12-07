b1aab6afa57651df76685c4db3aa1d703e1b624c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-07 18:50:05 -0500

    Cards

--- coffee-shop-react/src/components/Home.jsx
+++ coffee-shop-react/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import Cards from './Cards';
 import MainSection from './MainSection';
 
 
@@ -8,6 +9,7 @@ function Home() {
   return (
     <>
       <MainSection />
+      <Cards />
     </>
   )
 }
