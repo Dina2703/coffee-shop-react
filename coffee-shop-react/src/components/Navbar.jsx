2b7e57531a0b9c6d7b905e6120fe64625070e9f9
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-06 20:14:54 -0500

    Navbar starter

---
+++ coffee-shop-react/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+          COFFEE_DOM <i className="fa fa-coffee fa-2x" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? 'fa fa-times' : 'fa fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>Home</Link>
+            </li>
+            <li className="nav-item">
+            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>Services</Link>
+          </li>
+          <li className="nav-item">
+          <Link to="/products" className="nav-links" onClick={closeMobileMenu}>Product</Link>
+        </li>
+        <li className="nav-item">
+        <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>Sign Up</Link>
+      </li>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
